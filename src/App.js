3a039add22040c8ff616e6d8e72d05c2068b85e4
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-24 14:08:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
