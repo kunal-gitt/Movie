bae350219682407ad8f1d99ed9be77bbea38caee
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-24 14:37:15 +0530

    Deliverables for making api call using fetch

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
- 
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((moviedata) => {
+      return {
+        id: moviedata.episode_id,
+        title: moviedata.title,
+        openingText: moviedata.opening_crawl,
+        releaseDate: moviedata.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
