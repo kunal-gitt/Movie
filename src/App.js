d494b1be9eb49ea2f7582d4d3552d52cea964173
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-25 15:53:10 +0530

    Deliverables Storing and deleting the new movies into the Databases

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./Components/MovieList";
+import AddMovie from "./Components/Addmovie";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, seterror] = useState(null);
-  const [title, settitle] = useState("");
-  const [opening_text, setopeningtext] = useState("");
-  const [release_date, setreleasedate] = useState("");
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     seterror(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://react-http-bbcb8-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something went wrong...!");
       }
       const data = await response.json();
 
-      const transformedMovies = data.results.map((moviedata) => {
-        return {
-          id: moviedata.episode_id,
-          title: moviedata.title,
-          openingText: moviedata.opening_crawl,
-          releaseDate: moviedata.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedmovies = [];
+
+      for (const key in data) {
+        loadedmovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedmovies);
     } catch (error) {
       seterror(error.message);
     }
@@ -56,46 +59,51 @@ function App() {
     content = <p>Loading...</p>;
   }
 
-  const submithandler = (e) => {
-    e.preventDefault();
-    const NewMovieObj = {
-      title: title,
-      openingtext: opening_text,
-      releasedate: release_date,
-    };
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://react-http-bbcb8-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
 
-    console.log(NewMovieObj);
+    const data = await response.json();
+    console.log(data);
   };
 
-  const titlechange = (e) => {
-    settitle(e.target.value);
-  };
+  const deleteMovieHandler = async () => {
+    const response = await fetch(
+      "https://react-http-bbcb8-default-rtdb.firebaseio.com/movies.json/NTrqzZqz9HzlqumLn3m",
+      {
+        method: "DELETE",
+      }
+    );
+    const data = response.json();
 
-  const openingtext = (e) => {
-    setopeningtext(e.target.value);
-  };
+    const loadedmovies = [];
 
-  const releasedate = (e) => {
-    setreleasedate(e.target.value);
+    for (const key in data) {
+      loadedmovies.pop({
+        id: key,
+        title: data[key].title,
+        openingText: data[key].openingText,
+        releaseDate: data[key].releaseDate,
+      });
+    }
+
+    setMovies(loadedmovies);
   };
 
   return (
     <React.Fragment>
-      <form onSubmit={submithandler}>
-        <label>Title</label>
-        <input type="text" onChange={titlechange}></input>
-        <br />
-        <label>Opening Text</label>
-        <input type="text" onChange={openingtext}></input>
-        <br />
-        <label>Release Date</label>
-        <input type="text" onChange={releasedate}></input>
-        <br />
-        <button>Add Movie</button>
-        <br />
-      </form>
+      <AddMovie onAddMovie={addMovieHandler} />
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={deleteMovieHandler}>Delete Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
